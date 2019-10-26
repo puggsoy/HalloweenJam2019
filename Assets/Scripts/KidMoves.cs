a3805e90f28b6224beb2ecd24d58ae676951f2c4
OlivierYC <olivier.y@chrun.com>
2019-10-26 15:37:13 +1300

    Created kid movement scripts. Moved it to Scripts folder. Can move kid on x-axis with joystick in scene Olivier.

---
+++ Assets/Scripts/KidMoves.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidMoves : MonoBehaviour
+{
+    public float kidMoveSpeed;
+    private Rigidbody kidRigidBody;
+    private Vector3 kidVector3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kidMoveSpeed = 10f;
+        kidRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        kidVector3 = transform.TransformDirection(hAxis, 0, 0) * kidMoveSpeed * Time.deltaTime;
+        kidRigidBody.MovePosition(transform.position + kidVector3);
+    }
+
+}
