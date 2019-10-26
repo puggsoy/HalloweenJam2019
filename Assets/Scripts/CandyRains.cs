d5e237efe565a1c13037ea574a5b116e5fdafa70
OlivierYC <olivier.y@chrun.com>
2019-10-26 18:44:23 +1300

    Bouncy candy, slidey floors. Created Candy script

---
+++ Assets/Scripts/CandyRains.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyRains : MonoBehaviour
+{
+
+    private Rigidbody2D candyRigidBody;
+    public float candyVelocity;
+    private float candyRandomDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        candyRigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            throwCandy();
+        }
+        candyRandomDirection = Random.Range(-1f, 1f);
+
+
+    }
+
+    void createCandy()
+    {
+
+    }
+
+    void throwCandy()
+    {
+        candyVelocity = Random.Range(10f, 20f);
+        candyRigidBody.AddForce(transform.up * candyVelocity, ForceMode2D.Impulse);
+        candyVelocity = Random.Range(20f, 40f);
+        candyRigidBody.AddForce(transform.right * candyVelocity*candyRandomDirection, ForceMode2D.Impulse);
+
+    }
+
+}
