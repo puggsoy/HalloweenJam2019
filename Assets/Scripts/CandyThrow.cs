3fdf1ede35c65046f4efa3bb49e912cd8a790b9b
OlivierYC <olivier.y@chrun.com>
2019-10-26 21:20:23 +1300

    Candy prefab Started messing with tags and layers

--- Assets/Scripts/CandyThrow.cs
+++ Assets/Scripts/CandyThrow.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class CandyThrow : MonoBehaviour
 {
+
+    public int candyMin = 1;
+    private int candySpawns;
+    public int candyMax = 6;
+    public float candyMinSpeed = 1f;
+    public float candyMaxSpeed = 10f;
+    public float candyMinDirection;
+    public float candyMaxDirection;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +28,21 @@ public class CandyThrow : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Arm")
+        {
+            Debug.Log("Arm Throw");
+        }
+    }
+
+    void CreateCandy()
+    {
+        candySpawns = Random.Range(candyMin, candyMax);
+        Debug.Log(candySpawns);
+    }
+
+    void ThrowCandy(int howManyCandy)
+    {
 
     }
+
 }
