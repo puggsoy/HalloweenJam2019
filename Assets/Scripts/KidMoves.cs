e61a2ddd40bd16722e3c0190928c5c5e3e21a162
Sami Habib <puggsoy@gmail.com>
2019-10-26 22:06:18 +1300

    Added SamiScene and made kid animate via script

--- Assets/Scripts/KidMoves.cs
+++ Assets/Scripts/KidMoves.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class KidMoves : MonoBehaviour
 {
-    public float kidMoveSpeed;
+	public float kidMoveSpeed = 3;
     private Rigidbody2D kidRigidBody;
     private Vector2 kidVector2;
+	private Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
-        kidMoveSpeed = 10f;
         kidRigidBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float hAxis = Input.GetAxis("Horizontal");
         kidVector2 = transform.TransformDirection(hAxis, 0,0) * kidMoveSpeed * Time.deltaTime;
         Vector2 whereLegs = transform.position;
         kidRigidBody.MovePosition(whereLegs + kidVector2);
-    }
 
+		animator?.SetBool("OnOff", hAxis != 0);
+
+		Vector3 scale = transform.localScale;
+
+		if (hAxis > 0)
+		{
+			transform.localScale = new Vector2(Mathf.Abs(scale.x) * -1, scale.y);
+		}
+		else if (hAxis < 0)
+		{
+			transform.localScale = new Vector2(Mathf.Abs(scale.x) * 1, scale.y);
+		}
+	}
 }
