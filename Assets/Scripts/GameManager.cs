fa9478ed31e27d52b77dec40b838e70d1d57dc73
Sami Habib <puggsoy@gmail.com>
2019-10-27 22:32:53 +1300

    Added kid spawning code + fixed weird sprite issue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+	public int playerNum = 3;
+	public KidMoves[] kidPrefabs;
+
+	private KidMoves[] kids;
+
 	private void Awake()
 	{
-		
+		kids = new KidMoves[playerNum];
+	}
+
+	private void Start()
+	{
+		for (int i = 0; i < kids.Length; i++)
+		{
+			kids[i] = Instantiate<KidMoves>(kidPrefabs[i], new Vector2(i * 9, -9), Quaternion.identity, transform);
+			kids[i].kidNumber = i + 1;
+			kids[i].transform.localScale = new Vector2(1, 1);
+		}
 	}
 }
